32e163a53100d81af1c6459d4854000dc75f560f
derhaideer2 <muhammedemir.akguel@gmail.com>
2026-01-13 21:57:46 +0100

    Adding api fundementals

---
+++ app/Http/Controllers/PatientsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Patient;
+use Illuminate\Http\Request;
+
+class PatientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Patient::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(c $c)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(c $c)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, c $c)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(c $c)
+    {
+        //
+    }
+}
--- app/Models/Diseases.php
+++ app/Models/Diseases.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Disease extends Model
+class Diseases extends Model
 {
     protected $table = 'diseases';
     protected $primaryKey = 'disease_id';
